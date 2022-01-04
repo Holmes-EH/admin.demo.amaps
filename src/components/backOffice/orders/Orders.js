214645beebc8d5e800c0a36953e0f0e24fb79a71
holmes-tech <holmes.samuel@protonmail.com>
2022-01-04 10:49:42 +0100

    hide content on loading true

--- src/components/backOffice/orders/Orders.js
+++ src/components/backOffice/orders/Orders.js
@@ -261,149 +261,174 @@ const Orders = () => {
 			)}
 			{displayModal && <div>Display modal to edit</div>}
 			{message && <Toaster message={message} type={messageType} />}
-			{loading && <Loader />}
-			<div className='monthSelect'>
-				<BiChevronLeft className='changeDate' onClick={decrementDate} />
-				{selectedMonth.toLocaleDateString('fr-FR', {
-					month: 'long',
-					year: 'numeric',
-				})}
-				<BiChevronRight
-					className='changeDate'
-					onClick={incrementDate}
-				/>
-			</div>
-			{orders && orders.length !== 0 ? (
-				<table
-					style={{
-						minWidth: '780px',
-						maxWidth: '1100px',
-						margin: '0 auto',
-					}}
-				>
-					<thead>
-						<tr>
-							<th>Amap</th>
-							<th>Nom</th>
-							{products.map((product) => {
-								return (
-									<th key={product._id}>
-										<b>{product.title}</b>
-										<br />
-										<i>{product.pricePerKg.toFixed(2)}</i>
-									</th>
-								)
-							})}
-							<th>Total</th>
-							<th>Payé ?</th>
-						</tr>
-					</thead>
-					<tbody>
-						{orders.map((order, index) => {
-							return (
-								<tr key={order._id}>
-									<td>
-										<b>{order.amap.name}</b>
-									</td>
-
-									<td>
-										<a
-											href={`/clients/${order.client._id}`}
-										>
-											{order.client.name}
-										</a>
-									</td>
-
-									{products.map((product) => {
-										const detailToDisplay =
-											order.details.filter(
-												(detail) =>
-													detail.product._id ===
-													product._id
-											)
-
-										if (detailToDisplay.length !== 0) {
-											return (
-												<td
-													key={detailToDisplay[0]._id}
-												>
-													{
-														detailToDisplay[0]
-															.quantity
-													}
-												</td>
-											)
-										} else {
-											return (
-												<td
-													key={`${product._id}-notIn-${order._id}`}
-												>
-													0
-												</td>
-											)
-										}
-									})}
-
-									<td>
-										<b>
-											{order.details
-												.reduce(getOrderTotal, 0)
-												.toFixed(2)}{' '}
-											€
-										</b>
-									</td>
-									<td
-										className={
-											order.paid
-												? 'available'
-												: 'unavailable'
-										}
-										style={{
-											textAlign: 'center',
-											fontSize: '1.5em',
-										}}
-									>
-										{order.paid ? (
-											<BiCheckCircle
-												className='paidStatus'
-												onClick={() =>
-													changePaidStatus(index)
-												}
-											/>
-										) : (
-											<BiMinusCircle
-												className='paidStatus'
-												onClick={() =>
-													changePaidStatus(index)
-												}
-											/>
-										)}
-									</td>
-									<td className='rowEnd'>
-										<BiEdit
-											className='action'
-											onClick={() => {
-												editOrder(index, order)
-											}}
-										/>
-									</td>
-									<td className='rowEnd'>
-										<BiTrash
-											className='action'
-											onClick={() => {
-												deleteOrder(order._id)
-											}}
-										/>
-									</td>
-								</tr>
-							)
-						})}
-					</tbody>
-				</table>
+			{loading ? (
+				<Loader />
 			) : (
-				<p style={{ textAlign: 'center' }}>
-					Aucune Commande trouvée...
-				</p>
+				<>
+					<div className='monthSelect'>
+						<BiChevronLeft
+							className='changeDate'
+							onClick={decrementDate}
+						/>
+						{selectedMonth.toLocaleDateString('fr-FR', {
+							month: 'long',
+							year: 'numeric',
+						})}
+						<BiChevronRight
+							className='changeDate'
+							onClick={incrementDate}
+						/>
+					</div>
+					{orders && orders.length !== 0 ? (
+						<table
+							style={{
+								minWidth: '780px',
+								maxWidth: '1100px',
+								margin: '0 auto',
+							}}
+						>
+							<thead>
+								<tr>
+									<th>Amap</th>
+									<th>Nom</th>
+									{products.map((product) => {
+										return (
+											<th key={product._id}>
+												<b>{product.title}</b>
+												<br />
+												<i>
+													{product.pricePerKg.toFixed(
+														2
+													)}
+												</i>
+											</th>
+										)
+									})}
+									<th>Total</th>
+									<th>Payé ?</th>
+								</tr>
+							</thead>
+							<tbody>
+								{orders.map((order, index) => {
+									return (
+										<tr key={order._id}>
+											<td>
+												<b>{order.amap.name}</b>
+											</td>
+
+											<td>
+												<a
+													href={`/clients/${order.client._id}`}
+												>
+													{order.client.name}
+												</a>
+											</td>
+
+											{products.map((product) => {
+												const detailToDisplay =
+													order.details.filter(
+														(detail) =>
+															detail.product
+																._id ===
+															product._id
+													)
+
+												if (
+													detailToDisplay.length !== 0
+												) {
+													return (
+														<td
+															key={
+																detailToDisplay[0]
+																	._id
+															}
+														>
+															{
+																detailToDisplay[0]
+																	.quantity
+															}
+														</td>
+													)
+												} else {
+													return (
+														<td
+															key={`${product._id}-notIn-${order._id}`}
+														>
+															0
+														</td>
+													)
+												}
+											})}
+
+											<td>
+												<b>
+													{order.details
+														.reduce(
+															getOrderTotal,
+															0
+														)
+														.toFixed(2)}{' '}
+													€
+												</b>
+											</td>
+											<td
+												className={
+													order.paid
+														? 'available'
+														: 'unavailable'
+												}
+												style={{
+													textAlign: 'center',
+													fontSize: '1.5em',
+												}}
+											>
+												{order.paid ? (
+													<BiCheckCircle
+														className='paidStatus'
+														onClick={() =>
+															changePaidStatus(
+																index
+															)
+														}
+													/>
+												) : (
+													<BiMinusCircle
+														className='paidStatus'
+														onClick={() =>
+															changePaidStatus(
+																index
+															)
+														}
+													/>
+												)}
+											</td>
+											<td className='rowEnd'>
+												<BiEdit
+													className='action'
+													onClick={() => {
+														editOrder(index, order)
+													}}
+												/>
+											</td>
+											<td className='rowEnd'>
+												<BiTrash
+													className='action'
+													onClick={() => {
+														deleteOrder(order._id)
+													}}
+												/>
+											</td>
+										</tr>
+									)
+								})}
+							</tbody>
+						</table>
+					) : (
+						<p style={{ textAlign: 'center' }}>
+							Aucune Commande trouvée...
+						</p>
+					)}
+				</>
 			)}
 		</div>
 	)
